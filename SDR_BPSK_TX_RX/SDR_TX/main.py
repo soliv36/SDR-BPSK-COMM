1f31de052af17cfa74636df541f8adef85b6fe61
Sofia <116599758+soliv36@users.noreply.github.com>
2024-05-15 08:21:26 -0600

    Update main.py

--- SDR_BPSK_TX_RX/SDR_TX/main.py
+++ SDR_BPSK_TX_RX/SDR_TX/main.py
@@ -7,9 +7,9 @@ import sys
 sample_rate = 1e6
 center_freq = 915e6
 sdr = adi.Pluto("ip:192.168.2.1")
-
 tx.config_tx_pluto(sdr, sample_rate, center_freq, 20)
 
+#get the data from the user
 data = ''
 while True:
     data = input('Enter something (type / to quit): ')
@@ -17,6 +17,7 @@ while True:
     if data == '/':
         sys.exit()
 
+    #convert to binary and transmit
     data = tx.str_to_binary_list(input)
     tx.send_message(sdr, data, sample_rate)
 
