3fc5b9d004203ff0cb6361633529bd234527f5a4
Sofia <116599758+soliv36@users.noreply.github.com>
2024-05-15 17:47:17 -0600

    Update tx_rx.py

--- SDR_BPSK_/tx_rx.py
+++ SDR_BPSK_/tx_rx.py
@@ -1,13 +1,11 @@
 import adi
 import numpy as np
 
-#configure the Pluto SDR to transmit samples
 def config_tx(sdr, sample_rate, center_freq, tx_hardwaregain_chan0):
     sdr.tx_rf_bandwidth = int(sample_rate)
     sdr.tx_lo = int(center_freq)
     sdr.tx_hardwaregain_chan0 = int(tx_hardwaregain_chan0)
 
-#configure the Pluto SDR to receive samples
 def config_rx(sdr, sample_rate, center_freq, num_samples, gain_control_mode_chan0, rx_hardwaregain_chan0):
     sdr.rx_lo = int(center_freq)
     sdr.rx_rf_bandwidth = int(sample_rate)
@@ -15,7 +13,6 @@ def config_rx(sdr, sample_rate, center_freq, num_samples, gain_control_mode_chan
     sdr.gain_control_mode_chan0 = gain_control_mode_chan0
     sdr.rx_hardwaregain_chan0 = int(rx_hardwaregain_chan0)
 
-#transmit and receive samples at the same time
 def transmit_receive_cyclic(sdr, samples):
     sdr.tx_cyclic_buffer = True
     sdr.tx(samples)
@@ -25,11 +22,8 @@ def transmit_receive_cyclic(sdr, samples):
 
     rx_samples = sdr.rx()
     return rx_samples
-
-#add a sequence of bits before and after the data given
-def encode_bits(bits):
-    start_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1]
-    end_data =   [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    encoded = start_data + bits + end_data
-    return encoded
     
+def encode_bits(bits):
+    start_stop_bits = [1, 1, 1, 1, 1, 1, 1, 1]
+    encoded = start_stop_bits + bits + start_stop_bits
+    return encoded
